304fab7fa2386a3242a7c15143e8c4600bf3a7a3
devak997 <devakumar997@gmail.com>
2019-05-31 11:36:39 +0530

    Login Error Messages

--- src/Components/ErrorDisplay/ErrorDisplay.js
+++ src/Components/ErrorDisplay/ErrorDisplay.js
@@ -4,11 +4,15 @@ const ErrorDisplay = props => {
   return (
     <div className="ui negative message">
       <i className="close icon" onClick={props.handleXClick} />
-      <div className="header">We're sorry we can't connect to the server!</div>
+      <div className="header">{props.headerData}</div>
       <p>{props.message}</p>
-      <div className="ui blue button" onClick={props.tryAgain}>Try Again</div>
+      <div className="ui blue button" onClick={props.tryAgain} style={{display: props.showTry ? "" : "none"}}>Try Again</div>
     </div>
   );
 };
 
+const defaultProps = {
+  showTry: true,
+  headerData: "We're sorry we can't connect to the server!"
+}
 export default ErrorDisplay;
